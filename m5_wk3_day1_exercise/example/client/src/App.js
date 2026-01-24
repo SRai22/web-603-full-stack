fd63a1d432eb277a3400e79d30d39693090e1ea7
SRai22 <shravansomashekara.rai@gmail.com>
2026-01-24 11:59:18 -0800

    m5_wk3_day1_initial_commit

---
+++ m5_wk3_day1_exercise/example/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImplicitGrantExample from './ImplicitGrantExample';
+import AuthorizationCodeExample from './AuthorizationCodeExample';
+
+export default function App() {
+  return (
+    <div className="App">
+      <ImplicitGrantExample />
+      <AuthorizationCodeExample />
+    </div>
+  );
+}
